d398234827495f52484d3547b356b78eb28c3053
Victor Hugo <victor.h.s.reis@gmail.com>
2017-07-13 17:59:52 -0300

    2

--- sistema_login_simples_php/sistema_login/index.php
+++ sistema_login_simples_php/sistema_login/index.php
@@ -6,16 +6,11 @@ include('login/config.php');
 include('login/verifica_login.php');
 
 // Se não for permitido acesso nenhum ao arquivo
-// Inclua o trecho abaixo, ele redireciona o usuário para 
+// Inclua o trecho abaixo, ele redireciona o usuário para
 // o formulário de login
 include('login/redirect.php');
 ?>
 
 Olá <b><?php echo $_SESSION['nome_usuario']?></b>, <a href="login/sair.php">clique aqui</a> para sair.
 
-<p>Este arquivo é completamente bloqueado, caso queira bloquear apenas uma parte, remova
-<code>include('login/redirect.php');</code> e utilize o seguinte:</p>
-
-<pre>if ( $_SESSION['logado'] === true ) {
-	// Aqui vem a parte para usuários logados
-}</pre>
+<?php  ?>
